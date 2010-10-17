b20c4cc814ce783d6069184b6dafdb1109c65c0d
Christopher Wellons <mosquitopsu@gmail.com>
2010-10-17 19:48:55 -0400

    Added binding for insert-java-import in java-mode-plus.

--- java-mode-plus.el
+++ java-mode-plus.el
@@ -240,7 +240,8 @@ the given directory."
 	    (local-set-key "\C-xr" (create-ant-target "run"))
 	    (local-set-key "\C-xt" (create-ant-target "test"))
 	    (local-set-key "\C-xy" (create-ant-target "check"))
-	    (local-set-key "\C-xf" (create-ant-target "format"))))
+	    (local-set-key "\C-xf" (create-ant-target "format"))
+	    (local-set-key "\C-xI" 'insert-java-import))) ; from java-docs
 
 ;; This is here for the sake of the "run" Ant target above, so you can
 ;; see your program's output live.
